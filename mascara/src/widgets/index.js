70cbfd4e7dd11b6362a1c28cf166b4060c810f7a
frankiebee <frankie.diamond@gmail.com>
2017-10-20 01:29:55 -0700

    mascara/widget - clean up unused code

--- mascara/src/widgets/index.js
+++ mascara/src/widgets/index.js
@@ -1,6 +1,5 @@
 const render = require('react-dom').render
 const h = require('react-hyperscript')
-const EthQuery = require('ethjs-query')
 const options = {
   'mini-account': require('./mini-account')
 }
@@ -16,7 +15,6 @@ async function renderWidget (opts = {}) {
     limitedAccountManager,
   } = opts
   const Widget = options[type]
-  // const query = new EthQuery(provider)
   const state = await limitedAccountManager.getState()
   render(
     h(Widget, {
