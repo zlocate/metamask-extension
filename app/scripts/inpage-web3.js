2a7129b5ed43d94df8b94e198f70ade84968b8c8
bitpshr <mail@bitpshr.net>
2018-07-05 10:59:03 -0400

    Update e2e tests for approval

--- app/scripts/inpage-web3.js
+++ app/scripts/inpage-web3.js
@@ -1,3 +1,7 @@
 /* eslint-disable no-unused-vars */
 require('./lib/inpage-base.js')
 require('web3/dist/web3.min.js')
+window.web3 = new window.Web3(window.ETHEREUM_PROVIDER)
+window.ETHEREUM_PROVIDER.publicConfigStore.subscribe(({ selectedAddress }) => {
+  web3.eth.defaultAccount = selectedAddress
+})
